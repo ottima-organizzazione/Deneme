b3c11a39c2808ffc3d5e2999815a44a155d6d449
f6c5 <cicekf256@gmail.com>
2023-08-17 21:31:03 +0300

    controllers

---
+++ src/Core/Deneme.Application/Dtos/UpdateRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deneme.Application.Dtos
+{
+    public class UpdateRequest
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public int StockCount { get; set; }
+    }
+}
